5f696e143d024c97bf1a4106f12d97421c3733cf
System Administrator <root@MBP-de-Andre.home>
2023-09-21 15:08:58 +0100

    Adicionando estrutura inicial de pastas

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+//Definindo o template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//Definindo os arquivos públicos
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Habilitar server para receber dados via post (formulario)
+app.use(express.urlencoded({ extended: true }))
+
+//Rotas
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Titulo Teste'
+    })
+})
+
+// 404 error (not found)
+app.get('/', (req, res) => {
+    res.send('Página não encontrada')
+})
+
+//Executando o servidor
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
+
